0235599a94bde2e10bb7d3c1e6dda789199b1469
RachitGupta763 <rachitgupta763@gmail.com>
2024-05-04 13:17:42 +0530

    Version 2

---
+++ README.md
@@ -0,0 +1,10 @@
+# Become React Hero. 
+
+#Parcel
+-Local Server
+-HMR - Hot Module Replacement
+-File Watching Algorithm
+-Image Optimization
+-Minify & Compress
+-Bundling
+-Consistent Hashing
--- app.js
+++ app.js
@@ -1,21 +1,21 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const parent = React.createElement("div",{id:"parent"},
 [
     React.createElement("div",{id:"child1"},[
         React.createElement("h1",{},"Hello World H1 Tag"),
         React.createElement("h2",{},"Hello World H2 Tag")
-    ]
-    ),
+    ]),
     React.createElement("div",{id:"child2"},[
         React.createElement("h1",{},"Hello World H1 Tag"),
         React.createElement("h2",{},"Hello World H2 Tag")
-    ]
-    )
+    ]),
 ]
 );
 
-const heading = React.createElement("h1",{},"Hello World");
-console.log(heading);
-const root = document.getElementById("root");
-console.log(root);
 
-ReactDOM.render(parent, root);
+const root =ReactDOM.createRoot(document.getElementById("root"));
+console.log(parent);
+
+root.render(parent);
