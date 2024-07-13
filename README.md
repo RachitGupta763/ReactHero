b01d6e745ee35c2b03b9d714ea9ac12799925dd0
RachitGupta763 <rachitgupta763@gmail.com>
2024-07-13 15:36:33 +0530

    Version 3

--- README.md
+++ README.md
@@ -1,10 +1,17 @@
 # Become React Hero. 
 
-#Parcel
+**Parcel**
+-Dev Build
 -Local Server
 -HMR - Hot Module Replacement
--File Watching Algorithm
+-File Watching Algorithm-written in C++
 -Image Optimization
 -Minify & Compress
 -Bundling
 -Consistent Hashing
+-Differential Bundling-support older browser
+-Diagonostic
+-HTTPs
+-Error Handling
+-Tree Shaking ->Remove unUsed code ans Suffling 
+-differnt dev and production bundles
--- app.js
+++ app.js
@@ -1,21 +1,106 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent = React.createElement("div",{id:"parent"},
-[
-    React.createElement("div",{id:"child1"},[
-        React.createElement("h1",{},"Hello World H1 Tag"),
-        React.createElement("h2",{},"Hello World H2 Tag")
-    ]),
-    React.createElement("div",{id:"child2"},[
-        React.createElement("h1",{},"Hello World H1 Tag"),
-        React.createElement("h2",{},"Hello World H2 Tag")
-    ]),
-]
-);
+/**
+ * Header
+ * ->Logo
+ * ->Nav Section
+ * Body
+ * ->Search Bar
+ * ->Restaurant Items/Cards
+ *   ->image
+ *   ->name of res,dish,rating,price
+ * Footer
+ * ->Contact
+ * ->DisClaimer
+ */
+const Header = () => {
+    return (
+        <div className="header">
+            <div className="Logo-Container">
+                <img className="Logo"
+                src="https://graphicsfamily.com/wp-content/uploads/edd/2021/06/Editable-Photoshop-Food-Logo-Design-PNG-Transparent-1536x1536.png"
+                />
+            </div>
+            <div className="nav-item">
+                <ul>
+                <li>Home</li>
+                <li>About Us</li>
+                <li>Contact Us</li>
+                <li>Cart</li>
+                </ul>
+            </div>
+        </div>
+    );
+
+};
+const RestaurantCard = () =>{
+    return(
+      <div className="ResCard">
+        <img className="ResImg"
+                src="https://th.bing.com/th?q=Balushahi+Top+View&w=120&h=120&c=1&rs=1&qlt=90&cb=1&dpr=1.5&pid=InlineBlock&mkt=en-IN&cc=IN&setlang=en&adlt=moderate&t=1&mw=247"
+                />
+           <h3>Hatras BaluSahi</h3>
+           <h4>Sweet Indian</h4>
+           <h4>RS. 60</h4>
+           <h4>4.5 STAR</h4>
+      </div>
+    )
+}
+const Body = () => {
+    return(
+        <div className="Body">
+            <div className="Search">
+                Search
+            </div>
+            <div className="Res-Container">
+                <RestaurantCard />
+                <RestaurantCard />
+                <RestaurantCard />
+                <RestaurantCard />
+            </div>
+
+        </div>
+    )
+}
+const AppLayout = () =>{
+    return <div className="app">
+        <Header />
+        <Body />
+    </div>
+}
+
+const root=ReactDOM.createRoot(document.getElementById("root"));
+root.render(<AppLayout />);
+// //using core react fundamental
+// const heading = React.createElement("h1",{id:"heading"},"Namaste React");
+// console.log(heading);
 
 
-const root =ReactDOM.createRoot(document.getElementById("root"));
-console.log(parent);
+// //Now using Jsx
+// const JSXheading=(<h1 className="heading1">My name is Rachit Gupta</h1>);
+// console.log(JSXheading);
 
-root.render(parent);
+// //creating react funtional component with return function
+// // component have initial letter capital 
+
+// const add=200+400;
+// const Title = () =>{
+//     return(
+//         <div id="heading 1">
+//             {add};
+//             <h1>This is return functional component</h1>
+//         </div>
+//     )
+// }
+// // component Composition-> component inside another component
+// // {} ->this is use to write js inside component
+// const MainHeading =() => (
+//     <div id="heading 2">
+//      <Title /> 
+//      {Title()}
+//     <h1>This is without return functional component</h1>
+//     </div>
+// );
+
+
